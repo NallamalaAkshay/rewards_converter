ad702e8f749dc4f8f5629408023ef80fe1a95c9e
Akshay <113051732+NallamalaAkshay@users.noreply.github.com>
2023-05-03 14:48:41 +0530

    Revert "Reward-value"

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
@@ -9,25 +10,21 @@ public class RewardValueTests {
         var rewardValue = new RewardValue(cashValue);
         assertEquals(cashValue, rewardValue.getCashValue());
     }
+
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
         var rewardValue = new RewardValue(milesValue);
         assertEquals(milesValue, rewardValue.getMilesValue());
     }
+
     @Test
     void convert_from_cash_to_miles() {
-        double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        int expectedMiles = (int) Math.round(cashValue / 0.0035);
-        assertEquals(expectedMiles, rewardValue.getMilesValue());
+        assert false;
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        double expectedCash = milesValue * 0.0035;
-        assertEquals(expectedCash, rewardValue.getCashValue(), 0.01);
+        assert false;
     }
 }
