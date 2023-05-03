79da94f2d5164eee24e2abebc3f79b3cbb80f3d8
Akshay <akshaynallamala5068@gmail.com>
2023-05-03 14:26:36 +0530

    commited

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
@@ -21,16 +20,16 @@ public class RewardValueTests {
     @Test
     void convert_from_cash_to_miles() {
         double cashValue = 100;
-        int expectedMilesValue = (int) (cashValue / 0.0035);
         var rewardValue = new RewardValue(cashValue);
-        assertEquals(expectedMilesValue, rewardValue.getMilesValue());
+        int expectedMiles = (int) Math.round(cashValue / 0.0035);
+        assertEquals(expectedMiles, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
         int milesValue = 10000;
-        double expectedCashValue = milesValue * 0.0035;
         var rewardValue = new RewardValue(milesValue);
-        assertEquals(expectedCashValue, rewardValue.getCashValue());
+        double expectedCash = milesValue * 0.0035;
+        assertEquals(expectedCash, rewardValue.getCashValue(), 0.01);
     }
 }
